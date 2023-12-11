debd61c6c48e54781952066c4b53de0b0feaf3fd
ramiro <102697125+Ramiro-UTN@users.noreply.github.com>
2023-12-11 16:13:34 -0300

    Initial Commit | Create next app and add Clerk Auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      Guitar Shop Home Page
+    </div>
+  )
+}
---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Guitar Shop Admin Panel',
+  description: "Admin panel for Guitarras Lonatti's ecommerce",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
+}
