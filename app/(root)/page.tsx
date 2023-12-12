cce9f2da2c1d8a83cf1cc90487de4558b1a1df18
ramiro <102697125+Ramiro-UTN@users.noreply.github.com>
2023-12-12 14:17:43 -0300

    Add prisma and connected it to mysql db on planetscale

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
 import { UserButton } from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs";
 
 
 export default function Home() {
+  const {userId} = auth();
+ 
+  console.log("USER_ID", userId);
   return (
     <div className="flex justify-between items-center p-4 gap-x-2">
       Guitar Shop Admin Panel
